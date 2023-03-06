4beee0b0b586a1316ddc9979578262ed03851a6f
AyaOsama-pro <64667606+AyaOsama-pro@users.noreply.github.com>
2023-03-06 14:45:49 +0000

    Update WGCNA for astrocytes.R

--- WGCNA for astrocytes.R
+++ WGCNA for astrocytes.R
@@ -19,16 +19,14 @@ rb <- limma::removeBatchEffect(rl, batchg, design=design4)
 ##detecting outliers samples
 trb=t(rb)
 sampleTree2 = hclust(dist(trb), method = "average")
-##sample 6 is an outlier
 
-rb6=rb[,-6]
-
-madg=apply(rb6, 1, mad)
+##filter for low mad
+madg=apply(rb, 1, mad)
 mad1=as.matrix(madg)
 varb2=mad1[order(mad1[,1],decreasing=TRUE),]
 varb2=as.matrix(varb2)
 write.csv(varb2, "mad-gfap.csv")
-varb3=varb2[1:5000,]
+varb3=varb2[1:15000,]
 varb3=as.matrix(varb3)
 g3=c(row.names(varb3))
 rbg=rb6[g3,]
@@ -37,7 +35,7 @@ trbg=t(rbg)
 
 powers = c(c(1:10), seq(from = 12, to=20, by=2))
 # Call the network topology analysis function
-sft = pickSoftThreshold(trbg6, powerVector = powers, verbose = 5)
+sft = pickSoftThreshold(trbg, powerVector = powers, networktype= "signed", verbose = 5)
 # Plot the results:
 sizeGrWindow(9, 5)
 par(mfrow = c(1,2));
@@ -57,8 +55,8 @@ plot(sft$fitIndices[,1], sft$fitIndices[,5],
 text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
 
 
-net = blockwiseModules(trbg, power = 7,
-                       TOMType = "signed", minModuleSize = 20,
+net = blockwiseModules(trbg, power = 12,
+                       TOMType = "signed", minModuleSize = 15,
                        reassignThreshold = 0, mergeCutHeight = 0.25,
                        numericLabels = TRUE, pamRespectsDendro = FALSE,
                        saveTOMs = TRUE,
@@ -67,14 +65,14 @@ net = blockwiseModules(trbg, power = 7,
 ModuleColors2 = labels2colors(net$colors)
 nGenes = ncol(trbg);
 nSamples = nrow(trbg);
-MEs0 = moduleEigengenes(trbg6, ModuleColors2)$eigengenes
+MEs0 = moduleEigengenes(trbg, ModuleColors2)$eigengenes
+MEs= orderMEs(MEs0)
 
-
-NLF= factor(c(rep("-Away",5 ), rep("-Peri-Plaque", 6), rep("Trem2-Away", 4), rep("Trem2-Peri-plaque", 4)))
+NLF= factor(c(rep("-Away",6 ), rep("-Peri-Plaque", 6), rep("Trem2-Away", 4), rep("Trem2-Peri-plaque", 4)))
 design3=model.matrix(~0+NLF)
 
 moduleTraitCor = cor(MEs, design3, use = "p");
-write.csv(moduleTraitCor, "GFAP-batch-A6-5000-modultrait.csv")
+write.csv(moduleTraitCor, "GFAP-batch-15k-filtered-modultrait.csv")
 moduleTraitPvalue = corPvalueStudent(moduleTraitCor, nSamples);
 write.csv(moduleTraitPvalue, "GFAP-modules-pvalue.csv")
 # to display correlations and their p-values
